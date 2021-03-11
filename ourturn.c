a3439ac5db9d73b6a65d49b936c5501785ce1425
Federico Orsili <forsili@e3r9p3.42roma.it>
2021-03-11 15:06:14 +0100

    aggiusto il push VERSIONE BUONA

--- ourturn.c
+++ ourturn.c
@@ -6,7 +6,7 @@
 /*   By: forsili <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 12:55:06 by sgiovo            #+#    #+#             */
-/*   Updated: 2021/03/10 15:56:30 by forsili          ###   ########.fr       */
+/*   Updated: 2021/03/11 15:05:13 by forsili          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,19 @@ int		cd(char **argv, t_h *h)
 		argv[1] = ft_strdup("/Users/");
 		argv[1] = ft_strjoin(argv[1], h->usr);
 	}
+	errno = 0;
 	chdir(argv[1]);
+	return (errno);
+}
+
+int		ft_get_pwd(t_h *h)
+{
+	char	path[4096];
+	int		error;
+
+	errno = 0;
+	getcwd(path, FT_PATH_MAX);
+	printf("%s\n", path);
 	return (errno);
 }
 
@@ -30,6 +42,10 @@ int		ourturn_father(t_h *h, int i, char *cmd, char **argv)
 		h->error = cd(argv, h);
 		return (1);
 	}
+	if ((ft_strncmp(argv[0], "pwd", ft_strlen(argv[0])) == 0) && ft_strlen(argv[0]) == ft_strlen("pwd"))
+	{
+		h->error = ft_get_pwd(h);
+		return (1);
+	}
 	return (0);
 }
-
