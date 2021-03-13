4ce93ee576dccc7d9a126b121479d0864df5aca1
demian2435 <demian2435@gmail.com>
2021-03-13 11:32:17 +0100

    new splitter

--- splitterdot.c
+++ splitterdot.c
@@ -24,86 +24,65 @@ static void		ft_init_var(t_var_splitter *var)
 	var->start[0] = 0;
 }
 
-static void		ft_loop_bs(char *str, t_var_splitter *var)
-{
-	while (str[var->i] == '\\' && var->type_apice != '\'')
-	{
-		var->bs *= -1;
-		var->i++;
-		ft_loop_bs(str, var);
-		return;
-	}
-	if (var->bs == 1 && str[var->i])
-	{
-		var->bs = -1;
-		var->i++;
-		ft_loop_bs(str, var);
-		return;
-	}
-}
-
-static void		ft_loop_apici(char *str, t_var_splitter *var)
-{
-	if (var->type_apice == -1 && (str[var->i] == '\'' || str[var->i] == '\"'))
-	{
-		var->type_apice = str[var->i];
-		var->i++;
-		ft_loop_apici(str, var);
-		return;
-	}
-	if (var->type_apice == '\"')
-		ft_loop_bs(str, var);
-	if (var->type_apice != -1 && str[var->i] == var->type_apice)
-	{
-		var->type_apice = -1;
-		var->i++;
-		ft_loop_apici(str, var);
-		return;
-	}
-}
-
-static int		ft_control_split(char *str, char c, t_var_splitter *var)
-{
-	if (str[var->i] == c && var->type_apice == -1)
-	{
-		if (var->i == 0)
-			return (1);
-		var->stop[var->size] = var->i;
-		var->i++;
-		if (str[var->i] == c)
-			return (1);
-		if (str[var->i] != 0)
-		{
-			var->size++;
-			var->start[var->size] = var->i;
-		}
-	}
-	return (0);
-}
-
 char			**ft_splitter(char *str, char c)
 {
+	//DEBUG
+	printf("%sENTER SPLITTER '%c'%s\n", FGREEN, c, NONE);
+	// STOP DEBUG
+
 	t_var_splitter var;
 
 	ft_init_var(&var);
 	while (str[var.i])
 	{
-		ft_loop_apici(str, &var);
-		ft_loop_bs(str, &var);
-		if (ft_control_split(str, c, &var))
-			printf("ERRORE 1\n");
+		if (str[var.i] == '\\' && var.type_apice != '\'')
+		{
+			if (str[var.i + 1])
+				var.i += 2;
+			else
+			{
+				var.bs = 1;
+				break;
+			}
+		}
+		else if ((str[var.i] == '\'' || str[var.i] == '\"') && var.type_apice == -1)
+		{
+			var.type_apice = str[var.i];
+			var.i++;
+		}
+		else if (str[var.i] == var.type_apice)
+		{
+			var.type_apice = -1;
+			var.i++;
+		}
+		else if (str[var.i] == c && var.type_apice == -1)
+		{
+			var.stop[var.size] = var.i++;
+			while (str[var.i] == c && c == ' ')
+				var.i++;
+			var.start[++var.size] = var.i;
+		}
+		else
+			var.i++;
+	}
+	var.stop[var.size] = var.i;
+	if (var.type_apice != -1 || var.bs != -1)
+		printf("ERRORE\n");
+	if (!(var.matrix = malloc((var.size + 1) * sizeof(char *))))
+		return (NULL);
+	var.i = 0;
+	while (var.i <= var.size)
+	{
+		var.matrix[var.i] = ft_substr(str, var.start[var.i],(var.stop[var.i] - var.start[var.i]));
 		var.i++;
 	}
-	if (var.size == 0 && var.stop[0] == -1)
-		var.stop[0] = var.i;
-	if (var.type_apice != -1 || var.bs != -1)
-		printf("ERRORE 2\n");
-	if (!(var.matrix = malloc((var.size + 2) * sizeof(char *))))
-		return (NULL);
-	var.i = -1;
-	while (var.i++ < var.size)
-		var.matrix[var.i] = ft_substr(str, var.start[var.i],
-			(var.stop[var.i] - var.start[var.i]));
 	var.matrix[var.i] = 0;
+	
+	//DEBUG
+	for (int i = 0; var.matrix[i]; i++)
+		printf("%s%d = %.2d/%.2d || _%s_%s\n", FYELLOW,i, var.start[i], var.stop[i], var.matrix[i], NONE);
+	printf("%sEXIT SPLITTER%s\n", FRED, NONE);
+	//STOP DEBUG
+
 	return (var.matrix);
 }
